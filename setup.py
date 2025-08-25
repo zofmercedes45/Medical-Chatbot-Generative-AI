4d030ac802a872977f31995ba4d95af0883bdfec
Zofia <zosiazosieka@gmail.com>
2025-08-25 14:24:51 +0200

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Chatbot Project',
+    version= '0.0.0',
+    author= 'Zofia Pilitowska',
+    author_email= 'zofia.pilitowska1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
